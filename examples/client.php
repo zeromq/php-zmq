0cb3b39e3c26f851520819b419d1c15e0c855331
Mikko Koppanen <mkoppanen@gameboy.local>
2010-04-03 18:23:06 +0100

    Move draft.php -> examples/client.php

--- examples/client.php
+++ examples/client.php
@@ -1,19 +1,27 @@
 <?php
 
 /*
-    The client connects to the server, sends a message, 
-    receives a reply and then exits.
+    The client sends two messages using two different sockets
+    and then exits
 */
-$client = new ZeroMQ();
 
-$socket = new ZeroMQSocket(ZeroMQ::SOCKET_REQ);
-$socket->connect("tcp://127.0.0.1:5555");
+/* Create new queue object */
+$queue = new ZeroMQ();
 
-/* Assign socket */
-$client->setSocket($socket);
+/* Create persistent socket called 'MySock1' */
+$socket1 = new ZeroMQSocket(ZeroMQ::SOCKET_REQ, "MySock1");
+$socket1->connect("tcp://127.0.0.1:5555");
 
-/* Loop receiving and echoing back */
-$client->send("Hello there!");
-$message = $client->recv();
+/* Assign socket 1 to the queue, send and receive */
+var_dump($queue->setSocket($socket1)
+         ->send("hello there, using socket 1")
+         ->recv());
 
-var_dump($message);
+/* And another persistent socket called 'MySock2' */
+$socket1 = new ZeroMQSocket(ZeroMQ::SOCKET_REQ, "MySock2");
+$socket1->connect("tcp://127.0.0.1:5555");
+
+/* Assign socket 2 to the queue, send and receive */
+var_dump($queue->setSocket($socket1)
+         ->send("hello there, using socket 2")
+         ->recv());
