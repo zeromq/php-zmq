8ca779959bf088f20db40140c763dbebb0ff6cca
Sam Smith <git@samsmith.io>
2013-09-22 17:28:40 +0100

    "Manually install" the zmq extension.
    Currently, very nearly all tests are skipped during the Travis CI
    (see https://travis-ci.org/phuedx/php-zmq/jobs/11650067 and
    https://travis-ci.org/phuedx/php-zmq/jobs/11650068). The majority of
    tests are skipped because the zmq extension hasn't been loaded. Since
    manually installing the runkit extension during the Travis CI build for
    phuedx/pinkerton works, try manually installing the zmq extension here.

--- run-tests.sh
+++ run-tests.sh
@@ -2,15 +2,17 @@ wget http://download.zeromq.org/zeromq-2.2.0.tar.gz
 tar -xf zeromq-2.2.0.tar.gz
 cd ./zeromq-2.2.0
 ./autogen.sh
-./configure --prefix=/tmp/zeromq-2.2.0
-make install
+./configure
+make
+sudo make install
 cd ..
 phpize
-./configure --with-zmq=/tmp/zeromq-2.2.0
+./configure
 make
+sudo make install
+echo "extension=zmq.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 export NO_INTERACTION=1
 export REPORT_EXIT_STATUS=1
-# See https://github.com/reeze/php-leveldb/blob/master/travis/run-test.sh
 export TEST_PHP_EXECUTABLE=`which php`
 export USE_ZEND_ALLOC=0
-php run-tests.php -d=modules/zmq.so -m tests/*.phpt
+php run-tests.php -m ./tests/*.phpt
